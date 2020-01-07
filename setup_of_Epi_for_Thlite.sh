6fae2080321909564891f0d1434c8ee32ec9697a
Prabhas-Kumar <52374569+Prabhas-Kumar@users.noreply.github.com>
2020-01-08 02:45:38 +0530

    Added the .sh files of Bonus way

---
+++ setup_of_Epi_for_Thlite.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo pip3 install virtualenv
+python3 -m tflite1-env
+source tflite1-env/bin/activate
+bash get_pi_requirements.sh
---
+++ setup_of_Rpi_for_Thlite_part2.sh
@@ -0,0 +1,3 @@
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d Sample_TFLite_model
+
